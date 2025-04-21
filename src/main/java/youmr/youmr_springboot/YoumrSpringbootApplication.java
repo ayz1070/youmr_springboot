1833dde44f09c74d1bb8189e59b9cf0a2c369879
ayz1070 <ayz1070@gmail.com>
2025-04-21 22:05:35 +0900

    [init] 프로젝트 초기화

---
+++ src/main/java/youmr/youmr_springboot/YoumrSpringbootApplication.java
@@ -0,0 +1,13 @@
+package youmr.youmr_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoumrSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YoumrSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/youmr/youmr_springboot/YoumrSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package youmr.youmr_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoumrSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
