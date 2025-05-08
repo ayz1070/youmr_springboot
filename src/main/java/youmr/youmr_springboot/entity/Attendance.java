b02caf5be6d86cdda3b32fc1854233f691d0196a
Jun <80628496+ayz1070@users.noreply.github.com>
2025-05-08 16:44:58 +0900

    [entity] 이벤트 엔티티 생성 및 적용

--- src/main/java/youmr/youmr_springboot/entity/Attendance.java
+++ src/main/java/youmr/youmr_springboot/entity/Attendance.java
@@ -6,32 +6,36 @@ import lombok.*;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
-@Entity
-@Table(name = "attendance",
-        uniqueConstraints = @UniqueConstraint(columnNames = {"member_id", "event_id"})
-)
+
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Entity
+@Table(name = "attendance", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"member_id", "attendance_date", "attendance_type"}),
+        @UniqueConstraint(columnNames = {"member_id", "event_id"})
+})
 public class Attendance {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    // 필수
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "member_id")
+    @JoinColumn(name = "member_id", nullable = false)
     private Member member;
 
+    // 일일 출석일 경우 사용
+    private LocalDate attendanceDate;
+
+    // "DEFAULT", "EVENT" 같은 구분자
+    private String attendanceType;
+
+    // 이벤트 출석일 경우 사용
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "event_id")
     private Event event;
 
     private LocalDateTime createdAt;
-
-    @PrePersist
-    public void prePersist() {
-        this.createdAt = LocalDateTime.now();
-    }
 }
